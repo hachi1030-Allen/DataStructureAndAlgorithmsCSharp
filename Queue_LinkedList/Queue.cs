7242bec39fe7b92e483baf2e35e80773930f8942
Allen Liu <liu.allen1030@gmail.com>
2019-10-07 17:02:16 +0800

    Added Queue implementation with DotNet LinkedList, updated @ 2019/10/07 周一_17:02:16.44

---
+++ Queue_LinkedList/Queue.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queue_LinkedList
+{
+    public class Queue<T> : IEnumerable<T>
+    {
+        LinkedList<T> _items = new LinkedList<T>();
+        /// <summary>
+        /// Adds an item to the back of the queue
+        /// </summary>
+        /// <param name="item">Item been added</param>
+        public void Enqueue(T item)
+        {
+            _items.AddLast(item);
+        }
+        /// <summary>
+        /// Removes and returns the front item from the queue
+        /// </summary>
+        /// <returns>The front item from the queue</returns>
+        public T Dequeue()
+        {
+            // Handle if the queue is empty.
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+            // store the front item's value in a temporary variable
+            T value = _items.First.Value;
+            // remove the front item.
+            _items.RemoveFirst();
+            // return the front item value.
+            return value;
+        }
+        /// <summary>
+        /// Returns the front item from the queue without removing it from the queue
+        /// </summary>
+        /// <returns>The front item in the queue</returns>
+        public T Peek()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            return _items.First.Value;
+        }
+        /// <summary>
+        /// The number of items in the queue
+        /// </summary>
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+        /// <summary>
+        /// Remove all items from the queue
+        /// </summary>
+        public void Clear()
+        {
+            _items.Clear();
+        }
+        /// <summary>
+        /// Returns an enumerator that enumerates the queue
+        /// </summary>
+        /// <returns>The enumerator</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+        /// <summary>
+        /// Returns an enumerator that enumerates the queue
+        /// </summary>
+        /// <returns>The enumerator</returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+    }
+}
