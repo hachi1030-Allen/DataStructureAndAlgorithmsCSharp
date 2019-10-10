fe96c74964b1418cb4335a44c244083a4f86ddf6
Allen Liu <liu.allen1030@gmail.com>
2019-10-10 22:41:14 +0800

    Add HashTable implementation whose Nodes are based on LinkedList to avoid Collision, updated @ 2019/10/10 周四_22:41:14.64

--- HashTable/HashTable.cs
+++ HashTable/HashTable.cs
@@ -31,5 +31,174 @@ namespace HashTable
         {
 
         }
+        /// <summary>
+        /// Constructs a hash table with the default capacity
+        /// </summary>
+        /// <param name="initialCapacity">The initial capacity</param>
+        public HashTable(int initialCapacity)
+        {
+            if (initialCapacity < 1)
+            {
+                throw new ArgumentOutOfRangeException("initialCapacity");
+            }
+            _array = new HashTableArray<TKey, TValue>(initialCapacity);
+
+            // when the count exceeds this value, the next Add will cause the
+            // array to grow
+            _maxItemAtCurrentSize = (int)(initialCapacity * _fillFactor) + 1;
+        }
+        /// <summary>
+        /// Adds the key/value pair to the hash table. If the key already exists in the
+        /// hash table, an ArgumentException will be thrown
+        /// </summary>
+        /// <param name="key">The key of the item being added</param>
+        /// <param name="value">The value of the item being added</param>
+        public void Add(TKey key, TValue value)
+        {
+            // if we are at capacity, the array needs to grow
+            if (_count >= _maxItemAtCurrentSize)
+            {
+                // allocate a larger array
+                HashTableArray<TKey, TValue> largerArray = new HashTableArray<TKey, TValue>(_array.Capacity * 2);
+
+                // and re-add each item to the new array
+                foreach (HashTableNodePair<TKey, TValue> node in _array.Items)
+                {
+                    largerArray.Add(node.Key, node.Value);
+                }
+
+                // the larger array is now the hash table storage
+                _array = largerArray;
+
+                // update the new max items cached value
+                _maxItemAtCurrentSize = (int)(_array.Capacity * _fillFactor) + 1;
+            }
+            _array.Add(key, value);
+            _count++;
+        }
+        /// <summary>
+        /// Removes the item from the hash table whose key matches
+        /// the specified key.
+        /// </summary>
+        /// <param name="key">The key of the item to remove</param>
+        /// <returns>True if the item was removed, false otherwise</returns>
+        public bool Remove (TKey key)
+        {
+            bool removed = _array.Remove(key);
+            if (removed)
+            {
+                _count--;
+            }
+            return removed;
+        }
+        /// <summary>
+        /// Gets and sets the value with the specified key. ArgumentException is
+        /// thrown if the key does not already exist in the hash table.
+        /// </summary>
+        /// <param name="key">The key of the value to retrieve</param>
+        /// <returns>The value associated with the specified key</returns>
+        public TValue this[TKey key]
+        {
+            get {
+                TValue value;
+                if (!_array.TryGetValue(key, out value))
+                {
+                    throw new ArgumentException("key");
+                }
+                return value;
+            }
+            set
+            {
+                _array.Update(key, value);
+            }
+        }
+
+        /// <summary>
+        /// Finds and returns the value for the specified key.
+        /// </summary>
+        /// <param name="key">The key whose value is sought</param>
+        /// <param name="value">The value associated with the specified key</param>
+        /// <returns>True if the value was found, false otherwise</returns>
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _array.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// Returns a Boolean indicating if the hash table contains the specified key.
+        /// </summary>
+        /// <param name="key">The key whose existence is being tested</param>
+        /// <returns>True if the value exists in the hash table, false otherwise.</returns>
+        public bool ContainsKey(TKey key)
+        {
+            TValue value;
+            return _array.TryGetValue(key, out value);
+        }
+
+        /// <summary>
+        /// Returns a Boolean indicating if the hash table contains the specified value.
+        /// </summary>
+        /// <param name="value">The value whose existence is being tested</param>
+        /// <returns>True if the value exists in the hash table, false otherwise.</returns>
+        public bool ContainsValue(TValue value)
+        {
+            foreach (TValue foundValue in _array.Values)
+            {
+                if (value.Equals(foundValue))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Returns an enumerator for all of the keys in the hash table
+        /// </summary>
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                foreach (TKey key in _array.Keys)
+                {
+                    yield return key;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator for all of the values in the hash table
+        /// </summary>
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                foreach (TValue value in _array.Values)
+                {
+                    yield return value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes all items from the hash table
+        /// </summary>
+        public void Clear()
+        {
+            _array.Clear();
+            _count = 0;
+        }
+
+        /// <summary>
+        /// The number of items currently in the hash table
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
     }
 }
--- HashTable/HashTableArray.cs
+++ HashTable/HashTableArray.cs
@@ -36,5 +36,109 @@ namespace HashTable
         {
             _array[GetIndex(key)].Add(key, value);
         }
+        /// <summary>
+        /// Updates the value of the existing key/value pair in the node array.
+        /// If the key does not exist in the array an ArgumentException
+        /// will be thrown
+        /// </summary>
+        /// <param name="key">The key of the item being updated</param>
+        /// <param name="value">The updated value</param>
+        public void Update(TKey key, TValue value)
+        {
+            _array[GetIndex(key)].Update(key, value);
+        }
+        /// <summary>
+        /// Removes the item from the node array whose key matches
+        /// the specified key.
+        /// </summary>
+        /// <param name="key">The key of the item to remove</param>
+        /// <returns>True if the item was removed, otherwise false</returns>
+        public bool Remove(TKey key)
+        {
+            return _array[GetIndex(key)].Remove(key);
+        }
+        /// <summary>
+        /// Finds and returns the value of the specified key.
+        /// </summary>
+        /// <param name="key">The key whose value is sought</param>
+        /// <param name="value">The value associated with the specified key</param>
+        /// <returns>True if the value was found, otherwise false</returns>
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _array[GetIndex(key)].TryGetValue(key, out value);
+        }
+        /// <summary>
+        /// The capacity of the hash table array
+        /// </summary>
+        public int Capacity
+        {
+            get { return _array.Length; }
+        }
+        /// <summary>
+        /// Removes every item from the hash table array
+        /// </summary>
+        public void Clear()
+        {
+            foreach (HashTableArrayNode<TKey, TValue> node in _array)
+            {
+                node.Clear();
+            }
+        }
+        /// <summary>
+        /// Returns an enumerator for all the values in the node array
+        /// </summary>
+        public IEnumerable<TValue> Values
+        {
+            get
+            {
+                foreach (HashTableArrayNode<TKey, TValue> node in _array)
+                {
+                    foreach (TValue value in node.Values)
+                    {
+                        yield return value;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator for all of the keys in the node array
+        /// </summary>
+        public IEnumerable<TKey> Keys
+        {
+            get
+            {
+                foreach (HashTableArrayNode<TKey, TValue> node in _array)
+                {
+                    foreach (TKey key in node.Keys)
+                    {
+                        yield return key;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns an enumerator for all of the Items in the node array
+        /// </summary>
+        public IEnumerable<HashTableNodePair<TKey, TValue>> Items
+        {
+            get
+            {
+                foreach (HashTableArrayNode<TKey, TValue> node in _array)
+                {
+                    foreach (HashTableNodePair<TKey, TValue> pair in node.Items)
+                    {
+                        yield return pair;
+                    }
+                }
+            }
+        }
+
+        // Maps a key to the array index based on hash code
+        private int GetIndex(TKey key)
+        {
+            return Math.Abs(key.GetHashCode() % Capacity);
+        }
     }
 }
