bda5b19a48d0b8b47f5dd397ed4e15098ae7aec5
Deyun LIU <deyliu@microsoft.com>
2019-10-11 19:12:36 +0800

    Add Algorithms folder and implement first 2 exercise: LinkedListSearchMidNodes and MajorityElement normal version, updated @ Fri 10/11/2019_19:12:36.31

---
+++ Algorithms/CodeSamples/LinkedListSearchMidNode/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListSearchMidNode
+{
+    /// <summary>
+    /// This program is used to do a demonstration on a LinkedList Search Algorithm
+    /// Requirement: There is a LinkedList, try to find the mid item of the List, but
+    /// you can only go through the list once and you cannot use the Count property of
+    /// that LinkedList
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var oddList = new LinkedList<int>();
+            oddList.AddFirst(1);
+            oddList.AddFirst(2);
+            oddList.AddFirst(3);
+            oddList.AddFirst(4);
+            oddList.AddFirst(5);
+            Console.WriteLine("The middle item of the odd list is {0}", GetMidItem(oddList).Value);
+
+            var evenList = new LinkedList<int>();
+            evenList.AddFirst(1);
+            evenList.AddFirst(2);
+            evenList.AddFirst(3);
+            evenList.AddFirst(4);
+            evenList.AddFirst(5);
+            evenList.AddFirst(6);
+            evenList.AddFirst(7);
+            evenList.AddFirst(8);
+            Console.WriteLine("The middle item of the even list is {0}", GetMidItem(evenList).Value);
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// To do a sample, I will just assume that the LinkedList is based on Integer value type
+        /// </summary>
+        /// <param name="list">The LinkedList to search</param>
+        /// <returns>The Mid List Node</returns>
+        private static LinkedListNode<int> GetMidItem(LinkedList<int> list)
+        {
+            // For the detail explaination of the algorithm
+            // Refer to my own notes
+            var twoStepsPointer = list.First;
+            var oneStepsPointer = list.First;
+            while (twoStepsPointer.Next != null)
+            {
+                if (twoStepsPointer.Next.Next == null)
+                {
+                    return oneStepsPointer.Next;
+                }
+                else
+                {
+                    twoStepsPointer = twoStepsPointer.Next.Next;
+                    oneStepsPointer = oneStepsPointer.Next;
+                }
+            }
+            return oneStepsPointer;
+        }
+    }
+}
---
+++ Algorithms/CodeSamples/MajorityElement_Normal/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace FindNumFrequencyMoreThanHalfArraySize_Normal
+{
+    /// <summary>
+    /// This program is a demonstration on a normal algorithm which requies:
+    /// There is an array, all the elements are number. 
+    /// How to find the number whose frequency of appearance in the array is more than half of the total account of the array. 
+    /// Assume that there is definitely a number that matches  this condition.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[]{1,2,1,3,1};
+            Console.WriteLine("The number is {0}", GetTheNumber(array));
+        }
+
+        private static int? GetTheNumber(int[] array)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dic.ContainsKey(array[i]))
+                {
+                    dic[array[i]]++;
+                }
+                else
+                {
+                    dic.Add(array[i], 1);
+                }
+            }
+            // Can use LINQ but I choose to implement the logic here.
+            foreach (KeyValuePair<int, int> item in dic)
+            {
+                if (item.Value > (int)(array.Length / 2))
+                {
+                    return item.Key;
+                }
+            }
+            return null;
+        }
+    }
+}
