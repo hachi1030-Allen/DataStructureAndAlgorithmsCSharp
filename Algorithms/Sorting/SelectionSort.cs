98ff1747a7a5db74325c8201e41685aeebcad417
Allen Liu <liu.allen1030@gmail.com>
2019-10-12 21:06:36 +0800

    Add Selection Sort implementation, updated @ 2019/10/12 周六_21:06:36.03

---
+++ Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,43 @@
+using System;
+using Tracker;
+namespace Sorting
+{
+    public class SelectionSort<T>: Tracker<T>, ISorter<T>
+        where T: IComparable<T>
+    {
+        public void Sort(T[] items)
+        {
+            // starts with first index
+            // This index indicates where the unsorted part from.
+            int sortedRangeEnd = 0;
+            while (sortedRangeEnd < items.Length)
+            {
+                int nextIndex = FindIndexOfSmallestFromIndex(items, sortedRangeEnd);
+                // Swap the smallest item to the current sortedRangeEnd index.
+                Swap(items, sortedRangeEnd, items[nextIndex]);
+
+                sortedRangeEnd++;
+            }
+        }
+
+        private int FindIndexOfSmallestFromIndex(T[] items, int sortedRangeEnd)
+        {
+            // Set the currentSmallest to be the first element of the unsorted part.
+            // And also the currentSmallest index is pointing to the first index of unsorted part.
+            T currentSmallest = items[sortedRangeEnd];
+            int currentSmallestIndex = sortedRangeEnd;
+
+            for (int i = sortedRangeEnd + 1; i < items.Length; i++)
+            {
+                // replace the currentSmallest element whenever we found a smaller element.
+                // And also update the currentSmallestIndex to the smaller element.
+                if (Compare(currentSmallest, items[i]) > 0)
+                {
+                    currentSmallest = items[i];
+                    currentSmallestIndex = i;
+                }
+            }
+            return currentSmallestIndexl;
+        }
+    }
+}
