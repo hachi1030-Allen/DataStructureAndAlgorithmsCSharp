9f4cd3e073a779c71d70d8a89c07e6bd3e314f98
Allen Liu <liu.allen1030@gmail.com>
2019-10-13 08:33:29 +0800

    Add TestSorting project, updated @ 2019/10/13 周日_ 8:33:29.15

---
+++ Algorithms/TestProjects/TestSorting/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sorting;
+
+namespace TestSorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            int[] arr = new int[] {3,8,2,1,5,4,6,7};
+            MergeSort<int> sort = new MergeSort<int>();
+            sort.Sort(arr);
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
